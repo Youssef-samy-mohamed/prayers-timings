36f67d93a0535803c324a4966d9d567e750c17de
Youssef-samy-mohamed <samyyoussef813@gmail.com>
2025-03-11 04:07:01 +0200

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -182,22 +182,22 @@ export default function MainContent() {
         <Prayer
           name="ألظهر"
           time={timings.Dhuhr}
-          image="https://wepik.com/api/image/ai/9a07bb45-6a42-4145-b6aa-2470408a2921"
+          image="../../assets/imgs/dhhr-prayer-mosque.png"
         />
         <Prayer
           name="العصر"
           time={timings.Asr}
-          image="https://wepik.com/api/image/ai/9a07bb90-1edc-410f-a29a-d260a7751acf"
+          image="../../assets/imgs/asr-prayer-mosque.png"
         />
         <Prayer
           name="المغرب"
           time={timings.Sunset}
-          image="https://wepik.com/api/image/ai/9a07bbe3-4dd1-43b4-942e-1b2597d4e1b5"
+          image="../../assets/imgs/night-prayer-mosque.png"
         />
         <Prayer
           name="العشاء"
           time={timings.Isha}
-          image="https://wepik.com/api/image/ai/9a07bc25-1200-4873-8743-1c370e9eff4d"
+          image="../../assets/imgs/sunset-prayer-mosque.png"
         />
       </Stack>
       {/* select city */}
